58db7845d9a3a55946f6d61456f87a297d4f16ca
karbalhind <karbalhind0@gmail.com>
2024-01-15 14:13:58 +0100

    task27

---
+++ task27/revenous/src/components/App/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+import './App.css';
+
+const business={
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business,
+];
+
+
+class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    // Log d'un message avec les trois paramètres
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+
+
+
+render() {
+  return (
+    <div className="App">
+  <h1>ravenous</h1>
+  <SearchBar searchYelp={this.searchYelp}/>
+  <BusinessList businesses={businesses}/>
+</div>
+  );
+}
+}
+
+export default App;
