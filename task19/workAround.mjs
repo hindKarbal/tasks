77a9cd544b7db43832afe5520e2d0d7e5fd5ed7e
karbalhind <karbalhind0@gmail.com>
2024-01-15 13:26:25 +0100

    task19

---
+++ task19/workAround.mjs
@@ -0,0 +1,16 @@
+import { getCadre , calculateTax , getBenefits , calculateBonus , reimbursementEligibility  } from './employee.mjs';
+import employee from './employee.mjs';
+
+function getEmployeeInformation(inputSalary) {
+    employee.salary = inputSalary;
+    console.log('Cadre: ' + getCadre());
+    console.log('Tax: ' + calculateTax());
+    console.log('Benefits: ' + getBenefits());
+    console.log('Bonus: ' + calculateBonus());
+    console.log('Reimbursement Eligibility: ' + reimbursementEligibility() + '\n');
+  }
+  
+  getEmployeeInformation(10000);
+  getEmployeeInformation(50000);
+  getEmployeeInformation(100000);
+  
