bece1c332d59b6a3fd58b0718a96a52ea3af7adf
karbalhind <karbalhind0@gmail.com>
2024-01-15 14:09:13 +0100

    task25

---
+++ task25/revenous/src/components/BusinessList/BusinessList.js
@@ -0,0 +1,21 @@
+// BusinessList.js
+
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component {
+  render() {
+    const { businesses } = this.props;
+
+    return (
+      <div className="BusinessList">
+        {businesses.map((business) => {
+          return <Business  business={business} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default BusinessList;
